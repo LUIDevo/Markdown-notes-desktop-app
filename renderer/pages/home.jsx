25dfd346e0d24c0a9b388de19d1227dee3d68ba2
LUIDevo <sanicisspeedandfast@gmail.com>
2024-08-14 19:11:14 -0400

    making it work again :D|7;2u

--- renderer/pages/home.jsx
+++ renderer/pages/home.jsx
@@ -7,11 +7,13 @@ import TopBar from '../components/topbar';
 import Cookies from 'js-cookie';
 
 export default function HomePage() {
-  const [folderList, setFolderList] = useState([]); // State variable
-  const editorContent = "hi";
-  const selectedNote = "hi";
+  const [folderList, setFolderList] = useState([]); // List of folders fetched from server
+  const [visibleFolders, setVisibleFolders] = useState({}); // Track which folders are visible
+  const [folderContents, setFolderContents] = useState({}); // Store contents of each folder
+  const [editorContent, setEditorContent] = useState(''); // Content of the currently selected note
+  const [selectedNote, setSelectedNote] = useState(null); // Currently selected note
 
-useEffect(() => {
+  useEffect(() => {
     const fetchFolders = async () => {
       try {
         const response = await fetch('http://localhost:3001/getFolders');
@@ -23,46 +25,87 @@ useEffect(() => {
       }
     };
     fetchFolders();
-  }, []); 
+  }, []);
 
-  const handleJSONChange = () => {
-    console.log("hi");
+  const handleJSONChange = (newContent) => {
+    console.log("Content changed:", newContent);
+    setEditorContent(newContent);
   };
 
-  const getContentsOfNote = async (folder) => {
-      try {
-        const response = await fetch(`http://localhost:3001/getNotesInSubject/${folder}`)
-        const data = await response.json();
-        console.log("response", data)
-    } catch {
-      console.log("error")
-    }
-  }
+  const getContentsOfFolder = async (folderTitle) => {
+    try {
+      // Toggle visibility of folder contents
+      setVisibleFolders(prevState => ({
+        ...prevState,
+        [folderTitle]: !prevState[folderTitle],
+      }));
 
-    return (
-        <>
-        {folderList &&
-            folderList.map((folder,index) => {
-              return (
-              <div className="folderContainer">
-                  <div key={index} onClick={() =>getContentsOfNote(folder.title)} style={{color: "white"}}><svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.5 15V3H7.5L9 4.5H16.5V15H1.5Z" fill="#01BE58"/></svg>
-                  {folder.title}</div>
-              </div>
-              )
-        })
+      // Only fetch contents if the folder is not already visible
+      if (!visibleFolders[folderTitle]) {
+        const response = await fetch(`http://localhost:3001/getNotesInSubject/${folderTitle}`);
+        const data = await response.json();
+        console.log("Response data:", data);
+        setFolderContents(prevState => ({
+          ...prevState,
+          [folderTitle]: data,
+        }));
       }
-            <div className='flex'>
-                <SideBar
-                    folderList={folderList}
-                />
-                <div className="activity-container flex column">
-                    <MarkdownContainer
-                        onJSONChange={handleJSONChange}
-                        content={editorContent}
-                        selectednote={selectedNote ? selectedNote.title : "none"}
-                    />
-                </div>
+    } catch (error) {
+      console.error("Error fetching folder contents:", error);
+    }
+  };
+
+  return (
+    <>
+      {folderList &&
+        folderList.map((folder, index) => (
+          <div className="folderContainer" key={index}>
+            <div
+              onClick={() => getContentsOfFolder(folder.title)}
+              style={{ color: "white", cursor: "pointer" }}
+            >
+              <svg
+                width="18"
+                height="18"
+                viewBox="0 0 18 18"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M1.5 15V3H7.5L9 4.5H16.5V15H1.5Z" fill="#01BE58" />
+              </svg>
+              {folder.title}
             </div>
-        </>
-    );
+
+            {/* Conditionally render folder contents if visible */}
+            {visibleFolders[folder.title] && (
+              <div className="folderContents">
+                {folderContents[folder.title]?.map((file, fileIndex) => (
+                  <div
+                    key={fileIndex}
+                    onClick={() => {
+                      setSelectedNote(file);
+                      setEditorContent(file.content);
+                    }}
+                    style={{ color: "white", marginLeft: "20px", cursor: "pointer" }}
+                  >
+                    {file.title}
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+        ))
+      }
+      <div className='flex'>
+        <SideBar folderList={folderList} />
+        <div className="activity-container flex column">
+          <MarkdownContainer
+            onJSONChange={handleJSONChange}
+            content={editorContent}
+            selectednote={selectedNote ? selectedNote.title : "none"}
+          />
+        </div>
+      </div>
+    </>
+  );
 }
